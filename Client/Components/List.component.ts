8da26f85246db18c73893b6b3d267540b65e86a0
Ezeki Zibzibadze <ez@jok.io>
2016-04-05 11:02:49 +0400

    updated list component

--- Client/Components/List.component.ts
+++ Client/Components/List.component.ts
@@ -1,11 +1,11 @@
-﻿import {Component, OnInit, ElementRef, Input} from 'angular2/core';
+﻿import {Component, OnInit, ElementRef, Input, Output, EventEmitter} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router'
-
+import {Pagination} from './pagination.component'
 
 @Component({
     selector: 'list',
     templateUrl: '/html/Components/List.component.html',
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, Pagination]
 })
 export class ListComponent {
 
@@ -21,11 +21,62 @@ export class ListComponent {
     @Input()
     details_route: string[]
 
+    @Input()
+    paging: PagingConfig
+
+    @Output()
+    refreshData = new EventEmitter<RefreshInfo>()
+
+
+    sortKey: string
+    sortAsc: boolean
 
 
     hasLink(item: any, col: ListColumn) {
         return item && col && col.LinkRoute && col.LinkItemKey && item[col.LinkItemKey]
     }
+
+    sortClick(key: string) {
+
+        if (this.sortKey != key) {
+            this.sortKey = key
+            return
+        }
+
+        if (this.sortAsc == null || this.sortAsc == undefined)
+            this.sortAsc = false
+        else if (this.sortAsc === false)
+            this.sortAsc = true
+        else if (this.sortAsc === true)
+            this.sortAsc = null
+
+
+        console.log(key, this.sortAsc)
+
+        this.pageChanged()
+    }
+
+    pageChanged() {
+        this.refreshData.emit({
+            Skip: this.paging.ActivePage * this.paging.ItemsPerPage,
+            Take: this.paging.ItemsPerPage,
+            SortKey: this.sortKey,
+            SortAsc: this.sortAsc
+        })
+    }
+}
+
+export interface PagingConfig {
+    ItemsPerPage: number
+    ActivePage: number
+    TotalItemsCount: number
+}
+
+export interface RefreshInfo {
+    Skip: number
+    Take: number
+    SortKey?: string
+    SortAsc?: boolean
 }
 
 
--- Client/ServiceAgreement/List.ts
+++ Client/ServiceAgreement/List.ts
@@ -1,17 +1,26 @@
 ﻿import {Component, OnInit} from 'angular2/core';
-import {ListComponent, ListColumn} from '../Components/List.component'
+import {ListComponent, ListColumn, PagingConfig} from '../Components/List.component'
+import {Pagination} from '../Components/pagination.component'
 
 
 
 @Component({
     selector: 'list',
     templateUrl: '/html/ServiceAgreement/List.html',
-    directives: [ListComponent]
+    directives: [ListComponent, Pagination]
 })
 
 export class List {
 
-    // Configuration
+    // Paging Configuration
+    pagingConfig: PagingConfig = {
+        ActivePage: 1,
+        ItemsPerPage: 10,
+        TotalItemsCount: 2000
+    };
+
+
+    // Columns Configuration
     columns: ListColumn[] = [
         { Key: 'SLAAdminNumber', Name: 'SLAN', LinkRoute: ['/SLADetail'], LinkItemKey: 'SLAId' },
         { Key: 'OldCustomerNumber', Name: 'Old Customer Number' },
@@ -23,46 +32,53 @@ export class List {
         { Key: 'State' },
     ]
 
-
-
-    // Mock Data
-    items = [
-        {
-            SLAId: 1,
-            SLAAdminNumber: '3100003360',
-            OldCustomerNumber: '',
-            CustomerFullName: 'შპს რას მერჩი',
-            IdentificationNumber: '205205205',
-            District: 'აბაშა',
-            Address: 'ცცცც',
-            ServiceType: 'State Budget',
-            State: 'Active',
-        },
-        {
-            SLAId: 2,
-            SLAAdminNumber: '3100003360',
-            OldCustomerNumber: '123456',
-            CustomerFullName: 'ნინჩო ხელაძე 2+',
-            IdentificationNumber: '	01005007111',
-            District: 'აბაშა',
-            Address: 'ცცცც',
-            ServiceType: 'State Budget',
-            State: 'Active',
-        },
-        {
-            SLAId: 3,
-            SLAAdminNumber: '3100002586',
-            OldCustomerNumber: '123321',
-            CustomerFullName: '	რამანა რამანა',
-            IdentificationNumber: '01005007000',
-            District: 'ქარელი',
-            Address: 'ედიშერ მაღალაშვილის ქუჩა, N8',
-            ServiceType: 'Retail Commercial',
-            State: 'Suspended',
-        }
-    ]
+    items: any[]
 
 
 
+    constructor() {
+        this.refreshData({})
+    }
 
+
+    refreshData(info) {
+        console.log(info)
+
+
+        this.items = [
+            {
+                SLAId: 1,
+                SLAAdminNumber: '3100003' + Math.round(Math.random() * 1000),
+                OldCustomerNumber: '',
+                CustomerFullName: 'შპს რას მერჩი',
+                IdentificationNumber: '205205205',
+                District: 'აბაშა',
+                Address: 'ცცცც',
+                ServiceType: 'State Budget',
+                State: 'Active',
+            },
+            {
+                SLAId: 2,
+                SLAAdminNumber: '3100003' + Math.round(Math.random() * 1000),
+                OldCustomerNumber: '123456',
+                CustomerFullName: 'ნინჩო ხელაძე 2+',
+                IdentificationNumber: '	01005007111',
+                District: 'აბაშა',
+                Address: 'ცცცც',
+                ServiceType: 'State Budget',
+                State: 'Active',
+            },
+            {
+                SLAId: 3,
+                SLAAdminNumber: '3100002' + Math.round(Math.random() * 1000),
+                OldCustomerNumber: '123321',
+                CustomerFullName: '	რამანა რამანა',
+                IdentificationNumber: '01005007000',
+                District: 'ქარელი',
+                Address: 'ედიშერ მაღალაშვილის ქუჩა, N8',
+                ServiceType: 'Retail Commercial',
+                State: 'Suspended',
+            }
+        ]
+    }
 }
